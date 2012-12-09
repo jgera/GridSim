5e19e47aaf37a8b518797a83c9cb3fc6f580d363
Andrzej Tokarski <tokarski.andrzej@gmail.com>
2012-12-09 15:12:23 +0100

    UI switch

--- src/inz/MainWindow.java
+++ src/inz/MainWindow.java
@@ -1,28 +1,107 @@
 package inz;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
-import inz.model.StreetMap;
+import java.awt.EventQueue;
 
 import javax.swing.JFrame;
-import javax.swing.Timer;
+import javax.swing.BoxLayout;
+import java.awt.BorderLayout;
+import javax.swing.JPanel;
+import javax.swing.JTextPane;
+import javax.swing.JButton;
+import javax.swing.JScrollPane;
+import javax.swing.SwingConstants;
+import javax.swing.JLabel;
+import java.awt.Component;
+import java.awt.Button;
+import javax.swing.JToggleButton;
+import javax.swing.SpringLayout;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.ColumnSpec;
+import com.jgoodies.forms.layout.RowSpec;
+import com.jgoodies.forms.factories.FormFactory;
+import java.awt.Font;
 
-public class MainWindow extends JFrame {
-	
-	private static final long serialVersionUID = 3823711528095863438L;
+public class MainWindow {
 
-	public DrawPanel drawPanel;
-	
+	private JFrame mainFrame;
+	private DrawPanel simPanel;
+
+	/**
+	 * Create the application.
+	 */
 	public MainWindow() {
-
-		drawPanel = new DrawPanel();
-		add(drawPanel);
-
-		setTitle("Simple example");
-		setSize(800, 600);
-		setLocationRelativeTo(null);
-		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		initialize();
 	}
 
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		
+		mainFrame = new JFrame();
+		mainFrame.setBounds(100, 100, 489, 340);
+		mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		mainFrame.getContentPane().setLayout(new BorderLayout(0, 0));
+		
+		JTextPane consolePane = new JTextPane();
+		consolePane.setText(" \r\n \r\n \r\n ");
+		mainFrame.getContentPane().add(consolePane, BorderLayout.SOUTH);
+		
+		
+		simPanel = new DrawPanel();
+		mainFrame.getContentPane().add(simPanel, BorderLayout.CENTER);
+		
+		JPanel menuPanel = new JPanel();
+		mainFrame.getContentPane().add(menuPanel, BorderLayout.EAST);
+		menuPanel.setLayout(new FormLayout(new ColumnSpec[] {
+				ColumnSpec.decode("121px"),},
+			new RowSpec[] {
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,}));
+		
+		JLabel lblMenu = new JLabel("Menu");
+		lblMenu.setFont(new Font("Tahoma", Font.BOLD, 11));
+		menuPanel.add(lblMenu, "1, 2, center, center");
+		
+		JToggleButton tglbtnNewToggleButton = new JToggleButton("Average speed");
+		menuPanel.add(tglbtnNewToggleButton, "1, 4, fill, center");
+		
+		JToggleButton tglbtnNewToggleButton_1 = new JToggleButton("System input wait");
+		menuPanel.add(tglbtnNewToggleButton_1, "1, 6, fill, center");
+		
+		JToggleButton tglbtnCarsDetails = new JToggleButton("Cars details");
+		menuPanel.add(tglbtnCarsDetails, "1, 8");
+		
+		JToggleButton tglbtnNodeDetails = new JToggleButton("Node details");
+		menuPanel.add(tglbtnNodeDetails, "1, 10");
+		
+		JLabel lblSimulation = new JLabel("Simulation time");
+		lblSimulation.setFont(new Font("Tahoma", Font.BOLD, 11));
+		menuPanel.add(lblSimulation, "1, 12, center, default");
+		
+		JLabel lblTime = new JLabel("00:00:00");
+		menuPanel.add(lblTime, "1, 14, center, default");
+	}
+
+	public DrawPanel getSimPanel() {
+		return simPanel;
+	}
+	public JFrame getMainFrame() {
+		return mainFrame;
+	}
 }
--- src/inz/Starter.java
+++ src/inz/Starter.java
@@ -3,6 +3,8 @@ package inz;
 import inz.model.StreetMap;
 
 import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 
 public class Starter {
 
@@ -10,25 +12,38 @@ public class Starter {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (UnsupportedLookAndFeelException ex) {
+			System.out.println("Unable to load native look and feel");
+		} catch (ClassNotFoundException e) {
+			System.out.println("Unable to load native look and feel");
+		} catch (InstantiationException e) {
+			System.out.println("Unable to load native look and feel");
+		} catch (IllegalAccessException e) {
+			System.out.println("Unable to load native look and feel");
+		}
+
 		SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-            	StreetMap streetMap = MapHelpers.parseMap("data/simple.osm");
-        		
-        		MapHelpers.projectNodePoints(streetMap);
-        		MapHelpers.normaliseNodePositions(streetMap);
-        		MapHelpers.prepareMap(streetMap);
-        		MapHelpers.findLanesPositions(streetMap);
-                MapHelpers.findConnectorLengths(streetMap);
-                
-                MainWindow mainWindow = new MainWindow();
-                mainWindow.setVisible(true);
-                
-                SimRunner simRunner = new SimRunner(streetMap);
-        		simRunner.setDrawPanel(mainWindow.drawPanel);
-        		
-        		simRunner.startSimulation(50);
-            }
-        });
+			public void run() {
+				StreetMap streetMap = MapHelpers.parseMap("data/simple.osm");
+
+				MapHelpers.projectNodePoints(streetMap);
+				MapHelpers.normaliseNodePositions(streetMap);
+				MapHelpers.prepareMap(streetMap);
+				MapHelpers.findLanesPositions(streetMap);
+				MapHelpers.findConnectorLengths(streetMap);
+
+				MainWindow mainWindow = new MainWindow();
+				mainWindow.getMainFrame().setVisible(true);
+
+				SimRunner simRunner = new SimRunner(streetMap);
+				simRunner.setDrawPanel(mainWindow.getSimPanel());
+
+				simRunner.startSimulation(50);
+			}
+		});
 	}
 
 }
